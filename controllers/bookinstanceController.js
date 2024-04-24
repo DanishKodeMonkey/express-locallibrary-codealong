c76f3c3d81fa0d09ae63194eb67e0e954dc51c42
danishkodemonkey <humberjini@hotmail.com>
2024-04-24 17:32:59 +0200

    implement bookinstance detail view and controller

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -2,7 +2,6 @@ const BookInstance = require('../models/bookinstance');
 const asyncHandler = require('express-async-handler');
 
 // Display list of all BookInstances.
-// Display list of all BookInstances.
 exports.bookinstance_list = asyncHandler(async (req, res, next) => {
     const allBookInstances = await BookInstance.find().populate('book').exec();
 
@@ -14,7 +13,21 @@ exports.bookinstance_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific BookInstance.
 exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: BookInstance detail: ${req.params.id}`);
+    const bookInstance = await BookInstance.findById(req.params.id)
+        .populate('book')
+        .exec();
+
+    if (bookInstance === null) {
+        // No results.
+        const err = new Error('Book copy not found');
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render('bookinstance_detail', {
+        title: 'Book:',
+        bookinstance: bookInstance,
+    });
 });
 
 // Display BookInstance create form on GET.
