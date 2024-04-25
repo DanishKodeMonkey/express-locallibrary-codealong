2df0646bb8be5ccd801b1dd88d2c3dc1651a2a39
danishkodemonkey <humberjini@hotmail.com>
2024-04-25 16:05:20 +0200

    implement bookInstance_delete get/post

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -93,12 +93,20 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+    const bookInstance = await BookInstance.findById(req.params.id).exec();
+    if (bookInstance === null) {
+        res.redirect('/catalog/books');
+    }
+    res.render('bookinstance_delete', {
+        title: 'Delete book instance',
+        bookInstance: bookInstance,
+    });
 });
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+    await BookInstance.findByIdAndDelete(req.body.bookinstanceid);
+    res.redirect('/catalog/books');
 });
 
 // Display BookInstance update form on GET.
